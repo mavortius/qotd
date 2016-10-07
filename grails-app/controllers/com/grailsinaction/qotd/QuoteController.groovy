7d34a1fc5b2f04efa4790597eec8368b4f35939a
Marcelo Martins <m.a.martins@live.com>
2016-10-06 22:26:13 -0300

    Initial commit

---
+++ grails-app/controllers/com/grailsinaction/qotd/QuoteController.groovy
@@ -0,0 +1,19 @@
+package com.grailsinaction.qotd
+
+class QuoteController {
+
+    static defaultAction = "home"
+
+    def index() { }
+
+    def home() {
+        render "<h1>Real Programmers do not eat Quiche</h1>"
+    }
+
+    def random() {
+        def staticAuthor = "Anonymous"
+        def staticContent = "Real Programmers don't eat much quiche"
+
+        [author: staticAuthor, content: staticContent]
+    }
+}
---
+++ grails-app/views/quote/random.gsp
@@ -0,0 +1,13 @@
+<%@ page contentType="text/html;charset=UTF-8" %>
+<html>
+<head>
+    <title>Random Quote</title>
+</head>
+
+<body>
+<div id="quote">
+    <q>${content}</q>
+    <p>${author}</p>
+</div>
+</body>
+</html>
